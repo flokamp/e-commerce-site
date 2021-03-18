b3fc2d86e8b0d4d767f765aa218533d073357304
flokamp <74511633+flokamp@users.noreply.github.com>
2021-03-17 20:04:39 -0500

    add model columns

--- models/Tag.js
+++ models/Tag.js
@@ -1,20 +1,30 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 
-const sequelize = require('../config/connection.js');
+const sequelize = require("../config/connection.js");
 
 class Tag extends Model {}
 
 Tag.init(
-  {
-    // define columns
-  },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'tag',
-  }
+	{
+		id: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			primaryKey: true,
+			autoIncrement: true,
+		},
+		tag_name: {
+			type: DataTypes.STRING,
+		},
+	},
+	{
+		sequelize,
+		timestamps: false,
+		freezeTableName: true,
+		underscored: true,
+		modelName: "tag",
+	}
 );
+
+Category.hasMany(Category, { foreignKey: "category_id" });
 
 module.exports = Tag;
