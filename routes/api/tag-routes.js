b32714ef91d05aec3783c25576edaa1526c0db2f
flokamp <74511633+flokamp@users.noreply.github.com>
2021-03-23 23:13:06 -0500

    create product fixes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -66,7 +66,6 @@ router.post("/", (req, res) => {
 				});
 				return ProductTag.bulkCreate(tagProductIdArr);
 			}
-			// if no product tags, just respond
 			res.status(200).json(product);
 		})
 		.then((productTagIds) => res.status(200).json(productTagIds))
